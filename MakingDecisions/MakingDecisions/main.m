298a0a910510d2520518c34b14120503f015f117
Dasha <dasha.dolja@mail.ru>
2017-05-11 18:40:31 +0300

    add exercise 6.5

--- MakingDecisions/MakingDecisions/main.m
+++ MakingDecisions/MakingDecisions/main.m
@@ -9,61 +9,35 @@
 #import <Foundation/Foundation.h>
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-       long int n, r, count, sum, i, x;
-       NSMutableString *mutableString = [NSMutableString new];
-
-        NSLog (@"Enter number");
-        scanf ("%li", &n);
-        sum = 0;
-        count = 0;
-        x = n;
-        while (x != 0){
-            r = x % 10;
-            x = x / 10;
-            count += 1;
-            if (x == 0){
-               for (count = count -1; count >= 0; count = count -1){
-                   i = n / pow(10, count);
-                   n = n % (long int) pow(10, count);
-                   
-                   switch (i) {
-                       case 0:
-                           [mutableString appendString:@" Zero"];
-                           break;
-                       case 1:
-                           [mutableString appendString:@" One"];
-                           break;
-                       case 2:
-                           [mutableString appendString:@" Two"];
-                           break;
-                       case 3:
-                           [mutableString appendString:@" Three"];
-                           break;
-                       case 4:
-                           [mutableString appendString:@" Four"];
-                           break;
-                       case 5:
-                           [mutableString appendString:@" Five"];
-                           break;
-                       case 6:
-                           [mutableString appendString:@" Six"];
-                           break;
-                       case 7:
-                           [mutableString appendString:@" Seven"];
-                           break;
-                       case 8:
-                           [mutableString appendString:@" Eight"];
-                           break;
-                       case 9:
-                           [mutableString appendString:@" Ten"];
-                           break;
-                           
-                   }
-               }
-               NSLog(@"%@", mutableString);
-           }
+        int  number, right_digit;
+        NSMutableString *mutableString = [NSMutableString new];
+        
+        NSLog (@"Enter your number.");
+        scanf ("%i", &number);
+        
+        BOOL Negative = NO;
+        if (number < 0){
+            number = -number;
+            Negative = YES;
+        }
+        else if (number  == 0)
+            [mutableString appendString: @"Zero"];
+        
+        while (number != 0) {
+            right_digit = number % 10;
+            [mutableString appendString:[NSString stringWithFormat:@"%i", right_digit]];
+            number /= 10;
         }
         
+        
+        if (Negative)
+            [mutableString appendString:@"-"];
+        
+        NSLog(@"%@", mutableString);
+
+        
     }
+   
+    
     return 0;
 }
